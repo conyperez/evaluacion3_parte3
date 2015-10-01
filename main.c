a1fedc4f716d265ac9b873c0d9351208156c155e
Constanza Pérez Merry <conny.caamila@gmail.com>
2015-10-01 16:06:38 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,21 @@
+#ifndef STDIO_H
+#include <stdio.h>
+#define STDIO_H
+#endif
 
+#ifndef STDLIB_H
+#include <stdlib.h>
+#define STDLIB_H
+#endif
+
+#ifndef HASH_H
+#include "hash.h"
+#define HASH_H
+#endif
+
+
+int main(){
+
+
+    return 1;
+}
