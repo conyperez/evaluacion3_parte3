7fbf2da873c546e6f6c2e8c9e17c7d1b982fde8f
Constanza Pérez Merry <conny.caamila@gmail.com>
2015-10-03 22:50:12 -0300

    Update main.c

--- main.c
+++ main.c
@@ -8,6 +8,11 @@
 #define STDLIB_H
 #endif
 
+#ifndef STRING_H
+#include <string.h>
+#define STRING_H
+#endif
+
 #ifndef HASH_H
 #include "hash.h"
 #define HASH_H
@@ -21,22 +26,22 @@ int main(){
 
     inicializaRegistros();
 
-    insertarClave(1234);
-    insertarClave(5678);
-    insertarClave(6754);
-    insertarClave(5534);
-    insertarClave(1102);
-    insertarClave(1107);
+    insertarClave("1234");
+    insertarClave("5678");
+    insertarClave("6754");
+    insertarClave("5534");
+    insertarClave("1102");
+    insertarClave("1107");
 
     mostrarTabla();
 
-    if(buscarClave(6754) != -1){
+    if(buscarClave("6754") != -1){
         printf("\n\nClave encontrada");
     }else{
         printf("\n\nClave no encontrada");
     }
 
-    if(buscarClave(6987) != -1){
+    if(buscarClave("6987") != -1){
         printf("\n\nClave encontrada");
     }else{
         printf("\n\nClave no encontrada");
